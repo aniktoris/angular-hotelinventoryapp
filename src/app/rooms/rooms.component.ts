74e4dc7bd129afdba7f46d7db1f6610dcb6c2945
oleno <es.moname@gmail.com>
2024-04-18 15:14:17 +0200

    create rooms component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './rooms.component.scss'
 })
 export class RoomsComponent {
+  hotelName = 'Hilton Hotel';
+  numberOfRooms = 10;
 
+  hideRooms = false;
+  toggle(){
+    this.hideRooms = !this.hideRooms;
+  }
 }
