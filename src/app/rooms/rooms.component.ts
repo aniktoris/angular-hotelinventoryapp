21ce45c3f8f2ae5304e47dc4cca747250a0c2ee9
oleno <es.moname@gmail.com>
2024-04-18 16:17:34 +0200

    add roomList using ngFor

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -13,4 +14,39 @@ export class RoomsComponent {
   toggle(){
     this.hideRooms = !this.hideRooms;
   }
+
+  rooms: Room = {
+    totalRooms:20,
+    availableRooms:10,
+    bookedRooms:5
+  }
+
+  roomList: RoomList[] = [{
+    roomNumber: 1,
+    roomType: 'Deluxe Room',
+    amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
+    price: 500,
+    photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    checkinTime: new Date('11-Nov-2021'),
+    checkoutTime: new Date('12-Nov-2021')
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+      },
+    {
+      roomNumber: 3,
+        roomType: 'PrivateSuite',
+        amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 15000,
+        photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021')
+      }
+  ]
 }
