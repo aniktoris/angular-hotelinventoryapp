74b7265971ae258e8d2f291019a5bf4c22d2748e
oleno <es.moname@gmail.com>
2024-04-19 13:50:00 +0200

    add bootstrap and new room list component

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { RoomList } from '../rooms';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrl: './rooms-list.component.scss'
+})
+export class RoomsListComponent {
+
+  @Input() rooms: RoomList[] = [];
+
+}
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
 
 @Component({
@@ -6,13 +6,49 @@ import { Room, RoomList } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
 
   hideRooms = false;
+
   toggle(){
     this.hideRooms = !this.hideRooms;
+  }
+
+  roomList: RoomList[] = [];
+
+  ngOnInit(): void {
+    this.roomList = [{
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.5
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1000,
+        photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 3.6
+        },
+      {
+        roomNumber: 3,
+          roomType: 'PrivateSuite',
+          amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
+          price: 15000,
+          photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          checkinTime: new Date('11-Nov-2021'),
+          checkoutTime: new Date('12-Nov-2021'),
+          rating: 3.8
+        }]
   }
 
   rooms: Room = {
@@ -20,33 +56,4 @@ export class RoomsComponent {
     availableRooms:10,
     bookedRooms:5
   }
-
-  roomList: RoomList[] = [{
-    roomNumber: 1,
-    roomType: 'Deluxe Room',
-    amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
-    price: 500,
-    photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    checkinTime: new Date('11-Nov-2021'),
-    checkoutTime: new Date('12-Nov-2021')
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000,
-      photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021')
-      },
-    {
-      roomNumber: 3,
-        roomType: 'PrivateSuite',
-        amenities: 'Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 15000,
-        photos: 'https://images.unsplash.com/photo-1445991842772-097fea258e7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        checkinTime: new Date('11-Nov-2021'),
-        checkoutTime: new Date('12-Nov-2021')
-      }
-  ]
 }
