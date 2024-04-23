36ef9f98f77bffc66b83d9f8ac81087acd6d8ae0
oleno <es.moname@gmail.com>
2024-04-23 17:02:17 +0200

    add app_initializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +9,11 @@ import { ContainerComponent } from './container/container.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { requestInterceptor } from './request.interceptor';
+import { InitService } from './init.service';
 
+function initFactory(initService: InitService){
+  return () => initService.init();
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +32,14 @@ import { requestInterceptor } from './request.interceptor';
     provide: HTTP_INTERCEPTORS,
     useFactory: requestInterceptor,
     multi: true
-  }],
+  }, 
+// {
+//   provide: APP_INITIALIZER,
+//   useFactory: initFactory,
+//   deps: [InitService],
+//   multi: true
+// }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
