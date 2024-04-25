839d577580506f4d34aaa9333169f316a1427638
oleno <es.moname@gmail.com>
2024-04-25 15:20:10 +0200

    change element behaviour using ElementRef and Renderer2

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Directive, ElementRef, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
-export class HoverDirective {
+export class HoverDirective implements OnInit {
 
-  constructor() { }
+  colorB: string = 'pink';
+  color: string = 'darkblue';
+
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.element.nativeElement.style.backgroundColor = this.colorB;
+    this.renderer.setStyle(this.element.nativeElement, 'color', this.color);
+  }
 
 }
