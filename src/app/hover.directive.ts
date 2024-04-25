86877e225130e4c0d36562efd09ae44ab4c0178b
oleno <es.moname@gmail.com>
2024-04-25 15:37:47 +0200

    add HostListener to listen to event on component where the directive is applied

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,19 +1,26 @@
-import { DOCUMENT } from '@angular/common';
-import { Directive, ElementRef, Inject, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective implements OnInit {
 
-  colorB: string = 'pink';
+  //@Input() colorB: string = 'pink';
+  @Input() appHover: string = 'pink';
   color: string = 'darkblue';
 
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.element.nativeElement.style.backgroundColor = this.colorB;
+    this.element.nativeElement.style.backgroundColor = this.appHover;
     this.renderer.setStyle(this.element.nativeElement, 'color', this.color);
   }
 
+  @HostListener('mouseenter') onMouseEnter(){
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', "purple");
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', "white");
+  }
 }
